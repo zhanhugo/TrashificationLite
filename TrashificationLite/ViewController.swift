8074186c61e4419bcce7013910504988aa9f5739
zhanhugo <hugozhan0802@gmail.com>
2020-03-28 23:07:03 -0700

    Initial Commit

---
+++ TrashificationLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrashificationLite
+//
+//  Created by Hugo Zhan on 3/28/20.
+//  Copyright © 2020 Hugo Zhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
